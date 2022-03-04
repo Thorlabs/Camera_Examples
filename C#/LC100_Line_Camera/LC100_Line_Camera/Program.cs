042b6616592197e6f6620539b496ef8cf017daea
Matthew Anzelmi <manzelmi@Thorlabs.com>
2022-03-04 09:29:54 -0500

    Added Solution for CS Camera Examples in C#. 2 projects added

---
+++ C#/LC100_Line_Camera/LC100_Line_Camera/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LC100_Line_Camera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
